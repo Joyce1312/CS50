d133a3845c6e2e652266ed100d0b027684a400b1
Joyce Yeung <joyce1312183j@gmail.com>
2023-03-23 00:57:41 -0400

    Sequences and Data structures

--- Python/CS50.py
+++ Python/CS50.py
@@ -19,3 +19,38 @@ elif num < 0:
     print("Num is negative")
 else:
     print("Num is zero")
+
+#Sequences
+fName = "Joyce"
+#Will take the first letter (Starts counting from 0)
+print(fName[0])
+
+#Data Structures
+#List/Arrays (sequence of mutable (changable) values)
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+print (names)
+print(names[2])
+#Append function addeds a new value to the end of the list/array
+names.append("Draco")
+#Sort function sorts the list/array in alphabetical order
+names.sort()
+#Tuple (sequence of immutable (not changeable) values)
+coordinateX = 10.0
+coordinateY = 20.0
+#Example of a Tuple
+#Tuples uses paraentheses
+coordinate = (10.0, 20.0)
+#set (Collection of unique values)
+#Create empty set
+s = set()
+#Add function adds elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+#Remove function removes elements from the set
+s.remove(2)
+#Even if you have the same value more than once the value will only appear once in the set
+print(s)
+#len function will give you the length of a sequence (Tells you the number of thing in list, string, set, & etc)
+print(f"The set has {len(s)} elements.")
