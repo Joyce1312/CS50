1bced38b396fce442d59ffa67cc832280dd574cb
Joyce Yeung <joyce1312183j@gmail.com>
2023-03-23 00:24:46 -0400

    String concatenation and Formatted Strings

--- Python/CS50.py
+++ Python/CS50.py
@@ -6,3 +6,6 @@ name = input("Name: ")
 #Print Function
 #When + is used with strings it is called string concatenation
 print("Hello," + name)
+
+#F strings (AKA Formatted Strings)
+print(f"Hello, {name}")
